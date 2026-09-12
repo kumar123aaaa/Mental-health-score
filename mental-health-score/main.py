cde30d6bc9654a3c914f15a2d8e910c1e388e784
Aakash kumar <aakashyadav00696@gmail.com>
2026-09-13 00:11:57 +0530

    Update greeting message in main.py

--- mental-health-score/main.py
+++ mental-health-score/main.py
@@ -46,7 +46,7 @@ class PredictionResponse(BaseModel):
 
 @app.get('/')
 def greet():
-    return {'Welcome to Sheryians AI School Guys'}
+    return {'Welcome to Aakash kumar project'}
 
 
 @app.post('/predict', response_model=PredictionResponse) 
